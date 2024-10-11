288d39b91df168a58023fd025168329c9281f390
_maiten <maitenoviedo513@gmail.com>
2024-10-11 14:00:25 -0300

    Creacion de proyecto y primer commit

---
+++ src/main/java/org/example/parcial1magneto/Parcial1MagnetoApplication.java
@@ -0,0 +1,13 @@
+package org.example.parcial1magneto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial1MagnetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Parcial1MagnetoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/parcial1magneto/Parcial1MagnetoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parcial1magneto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial1MagnetoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
